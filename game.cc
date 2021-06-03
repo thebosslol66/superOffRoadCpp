6af8f660766a47126af087d58670351a1a5f04b9
heldael <estel56@outlook.fr>
2021-06-03 19:28:31 +0200

    sprite des voiture

--- game.cc
+++ game.cc
@@ -40,7 +40,7 @@
 using namespace std;
 using namespace sf;
 
-const bool DEBUG = false;
+const bool DEBUG = true;
 
 /*
  * Ce morceau de code pour permet de tirer un nombre flottant au hasard
@@ -225,6 +225,10 @@ struct Assets {
   sf::Music goalScreenmusic;
   sf::Music celebrationScreenmusic;
   sf::Music gameoverScreenmusic;
+
+  //Voiture
+  sf::Texture carTexture;
+  sf::Sprite carSprite;
 
 };
 
@@ -1169,8 +1173,8 @@ int main() {
   
   Ground level;
   Ground levelEmpty;
-  makeLevel(level, levelFile+ to_string(idLevel) + ".txt");
-  loadFromFile(assets.backgroundLevelScreenTexture, assets.backgroundLevelScreen, levelFile+to_string(idLevel) + ".png");
+  makeLevel(level, levelFile+ to_string(idLevel%4+1) + ".txt");
+  loadFromFile(assets.backgroundLevelScreenTexture, assets.backgroundLevelScreen, levelFile+to_string(idLevel%4+1) + ".png");
   loadFromFile(assets.backgroundMainScreenTexture, assets.backgroundMainScreen, "assets/fond_ecran_principal.png");
 
   loadFromFile(assets.superoffroadTextTexture, assets.superoffroadText, "assets/title_screen.png");
@@ -1243,6 +1247,7 @@ int main() {
   loadFromFile(assets.noUpgrade1Texture, assets.noUpgrade1Screen, "assets/noUpgrade1.png");
   loadFromFile(assets.noUpgrade2Texture, assets.noUpgrade2, "assets/noUpgrade2.png");
   loadFromFile(assets.showUpgradeTexture, assets.showUpgrade, "assets/showUpgrade.png");
+  loadFromFile(assets.carTexture, assets.carSprite, "assets/voiture.png");
   
   
   
@@ -2436,23 +2441,22 @@ int main() {
         }
       }
 
-      sf::RectangleShape carShape(sf::Vector2f(CAR_LONGUEUR, CAR_HAUTEUR));
 
       for (int j = 0; j < Enemies.size(); j++) {
         Car * enemie = Enemies[j];
-        carShape.setPosition(enemie -> pos.x, enemie -> pos.y);
-        carShape.setOrigin(CAR_LONGUEUR / 2, CAR_HAUTEUR / 2);
-        carShape.setRotation(180 - (enemie -> direction / 16.0 * 360));
-        carShape.setFillColor(enemie -> color);
-        window.draw(carShape);
+        assets.carSprite.setPosition(enemie -> pos.x, enemie -> pos.y);
+        assets.carSprite.setOrigin(CAR_LONGUEUR / 2, CAR_HAUTEUR / 2);
+        assets.carSprite.setRotation(180 - (enemie -> direction / 16.0 * 360));
+        assets.carSprite.setColor(enemie -> color);
+        window.draw(assets.carSprite);
       }
 
-      carShape.setPosition(playerCar.pos.x, playerCar.pos.y);
-      carShape.setOrigin(CAR_LONGUEUR / 2, CAR_HAUTEUR / 2);
-      carShape.setRotation(180 - (playerCar.direction / 16.0 * 360));
-      carShape.setFillColor(playerCar.color);
+      assets.carSprite.setPosition(playerCar.pos.x, playerCar.pos.y);
+      assets.carSprite.setOrigin(CAR_LONGUEUR / 2, CAR_HAUTEUR / 2);
+      assets.carSprite.setRotation(180 - (playerCar.direction / 16.0 * 360));
+      assets.carSprite.setColor(playerCar.color);
 
-      window.draw(carShape);
+      window.draw(assets.carSprite);
 
       if (DEBUG) {
     	  sf::CircleShape nitroShape;
